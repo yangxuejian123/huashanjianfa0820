0cdc4f29e5becfec27b2235f40acf6580e17811c
yangxuejian123 <yangxuejian123@qq.com>
2020-12-14 19:53:31 +0800

    修改

--- src/main/java/com/atguigu/git/Test.java
+++ src/main/java/com/atguigu/git/Test.java
@@ -3,5 +3,8 @@ package com.atguigu.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("这是一个测试类！！！");
+        System.out.println("这是一个测试类！！！");
+        System.out.println("这是一个测试类！！！");
+        System.out.println("这是一个测试类！！！");
     }
 }
